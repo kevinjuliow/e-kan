461e6078d16bccbc76fd12e60b6ade38c99b0f2e
Kevin Julio <kevjuliow@gmail.com>
2024-10-19 12:24:13 +0700

    Created dto for penjual

--- backend/src/main/java/com/example/backend/dtos/penjualDtos/PenjualDto.java
+++ backend/src/main/java/com/example/backend/dtos/penjualDtos/PenjualDto.java
@@ -1,4 +1,21 @@
 package com.example.backend.dtos.penjualDtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PenjualDto {
+    private UUID id_penjual ;
+    private String nama ;
+    private String email ;
+    private String website ;
+    private String alamat ;
+    private String no_telp ;
+    private Date createdAt ;
+    private Date updatedAt ;
 }
--- backend/src/main/java/com/example/backend/dtos/penjualDtos/RegisterPenjualDto.java
+++ backend/src/main/java/com/example/backend/dtos/penjualDtos/RegisterPenjualDto.java
@@ -1,4 +1,14 @@
 package com.example.backend.dtos.penjualDtos;
 
+import lombok.Data;
+
+import java.util.Date;
+@Data
 public class RegisterPenjualDto {
+    private String email ;
+    private String password ;
+    private String nama ;
+    private String no_telp;
+    private String alamat ;
+    private String website ;
 }
