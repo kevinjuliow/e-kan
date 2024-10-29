a2dbe8b4508151c00c7b70cfd55f9384b041cd39
stevanryan <stevanryan85@gmail.com>
2024-10-29 15:50:49 +0700

    Adding page and components and navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,22 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
+import { League_Spartan } from "next/font/google";
 import "./globals.css";
+import { Provider } from "./providers";
+import Navbar from "./components/Navbar/Navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const leagueSpartan = League_Spartan({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +30,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <Provider>
+        <body className={`${leagueSpartan.className} bg-[#D4EBEF]`}>
+          <Navbar />
+          <main className="flex flex-wrap flex-col items-center justify-between mx-auto w-full">
+            {children}
+          </main>
+        </body>
+      </Provider>
     </html>
   );
 }
