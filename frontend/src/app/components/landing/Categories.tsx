57efacc24c506b8e0ba24f3c430845ee4cb75bb5
stevanryan <stevanryan85@gmail.com>
2024-11-24 11:54:20 +0700

    Using Image from next

--- frontend/src/app/components/landing/Categories.tsx
+++ frontend/src/app/components/landing/Categories.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Categories = () => {
@@ -8,15 +9,15 @@ const Categories = () => {
         <p className="mt-2 text-xl text-darkaqua text-center">Kami menjual tiga jenis ikan yang dapat Anda pilih!</p>
         <div className="flex items-center justify-center w-full mt-12">
           <div className="flex flex-col items-center justify-center">
-            <img src={"/benih.jpg"} alt="benih" className="w-[200px] rounded-full custom-categories-image-style" />
+            <Image src={"/benih.jpg"} alt="benih" width={200} height={200} className="rounded-full custom-categories-image-style" />
             <p className="mt-4 text-xl text-darkaqua">Benih</p>
           </div>
           <div className="flex flex-col items-center justify-center mx-6 md:mx-12 lg:mx-16">
-            <img src={"/caloninduk.jpg"} alt="benih" className="w-[200px] rounded-full custom-categories-image-style" />
+            <Image src={"/caloninduk.jpg"} alt="benih" width={200} height={200} className="rounded-full custom-categories-image-style" />
             <p className="mt-4 text-xl text-darkaqua">Calon Induk</p>
           </div>
           <div className="flex flex-col items-center justify-center">
-            <img src={"/induk.jpg"} alt="benih" className="w-[200px] rounded-full custom-categories-image-style" />
+            <Image src={"/induk.jpg"} alt="benih" width={200} height={200} className="rounded-full custom-categories-image-style" />
             <p className="mt-4 text-xl text-darkaqua">Induk</p>
           </div>
         </div>
