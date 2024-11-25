a6b2cbe84f390e9b92572e304049ea5936230bd8
Kevin Julio <kevjuliow@gmail.com>
2024-11-25 20:35:41 +0700

    updated Alamat

--- backend/src/main/java/com/example/backend/services/AlamatPembeliService.java
+++ backend/src/main/java/com/example/backend/services/AlamatPembeliService.java
@@ -1,5 +1,6 @@
 package com.example.backend.services;
 
+import com.example.backend.Exceptions.GlobalExceptionHandler;
 import com.example.backend.models.AlamatPembeliModel;
 import com.example.backend.models.PembeliModel;
 import com.example.backend.repositories.AlamatPembeliRepo;
@@ -15,10 +16,54 @@ public class AlamatPembeliService {
     private final AlamatPembeliRepo repo ;
 
     public List<AlamatPembeliModel> findByPembeli (PembeliModel id) {
-        return repo.findByPembeli(id).get();
+        return repo.findByPembeli(id).orElseThrow(
+                ()-> new GlobalExceptionHandler.ResourceNotFoundException("Pembeli not found")
+        );
     }
 
     public AlamatPembeliModel saveAlamat(AlamatPembeliModel input) {
         return repo.save(input);
     }
+
+    public AlamatPembeliModel getById (UUID id ) {
+        return repo.findById(id).orElseThrow(
+                ()-> new GlobalExceptionHandler.ResourceNotFoundException("alamat id not found")
+        );
+    }
+
+    public AlamatPembeliModel update(AlamatPembeliModel input, UUID id) {
+        AlamatPembeliModel existingAlamat = repo.findById(id).orElseThrow(
+                () -> new GlobalExceptionHandler.ResourceNotFoundException("Alamat ID not found")
+        );
+
+        if (!existingAlamat.getAlamat_lengkap().equals(input.getAlamat_lengkap())) {
+            existingAlamat.setAlamat_lengkap(input.getAlamat_lengkap());
+        }
+        if (!existingAlamat.getKode_pos().equals(input.getKode_pos())) {
+            existingAlamat.setKode_pos(input.getKode_pos());
+        }
+        if (!existingAlamat.getKota().equals(input.getKota())) {
+            existingAlamat.setKota(input.getKota());
+        }
+        if (!existingAlamat.getProvinsi().equals(input.getProvinsi())) {
+            existingAlamat.setProvinsi(input.getProvinsi());
+        }
+        if (!existingAlamat.getKabupaten().equals(input.getKabupaten())) {
+            existingAlamat.setKabupaten(input.getKabupaten());
+        }
+        if (input.getKeterangan() != null && !input.getKeterangan().equals(existingAlamat.getKeterangan())) {
+            existingAlamat.setKeterangan(input.getKeterangan());
+        }
+
+        return repo.save(existingAlamat);
+    }
+
+    public void delete (UUID id) {
+        AlamatPembeliModel existingAlamat = repo.findById(id).orElseThrow(
+                () -> new GlobalExceptionHandler.ResourceNotFoundException("Alamat ID not found")
+        );
+
+        repo.delete(existingAlamat);
+    }
+
 }
