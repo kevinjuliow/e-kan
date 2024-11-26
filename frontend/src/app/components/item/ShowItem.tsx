ddc77936e7d0372cea60e405f8f85522151aaea5
stevanryan <stevanryan85@gmail.com>
2024-11-26 11:25:20 +0700

    Dashboard search bar works

--- frontend/src/app/components/item/ShowItem.tsx
+++ frontend/src/app/components/item/ShowItem.tsx
@@ -3,7 +3,11 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import EachItem from './EachItem'
 
-const ShowItem: React.FC = () => {
+interface ShowItemProps {
+  searchQuery: string
+}
+
+const ShowItem: React.FC<ShowItemProps> = ({ searchQuery }) => {
   const [itemData, setItemData] = useState<Item[]>([])
 
   useEffect(() => {
@@ -30,9 +34,14 @@ const ShowItem: React.FC = () => {
     console.log({ itemData });
   }, [itemData]);
 
+  // Filter the items based on searchQuery
+  const filteredItems = itemData.filter((item) =>
+    item.nama.toLowerCase().includes(searchQuery.toLowerCase()) // Filter by item name
+  )
+
   return (
     <div className={`grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 items-center justify-center w-full mt-12 ${!itemData && 'h-full mt-56 grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1'}`}>
-      {itemData?.map((item, index) => {
+      {filteredItems.length > 0 && filteredItems.map((item, index) => {
         return(
           <EachItem key={index} data={item} />
         )
