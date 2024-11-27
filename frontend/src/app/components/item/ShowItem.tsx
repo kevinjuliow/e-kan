c0b336f1579f2638931180b9ba5834ae059cd5fd
stevanryan <stevanryan85@gmail.com>
2024-11-27 12:24:48 +0700

    Fixed error when no items are available

--- frontend/src/app/components/item/ShowItem.tsx
+++ frontend/src/app/components/item/ShowItem.tsx
@@ -35,20 +35,19 @@ const ShowItem: React.FC<ShowItemProps> = ({ searchQuery }) => {
   }, [itemData]);
 
   // Filter the items based on searchQuery
-  const filteredItems = itemData.filter((item) =>
+  const filteredItems = itemData && itemData.filter((item) =>
     item.nama.toLowerCase().includes(searchQuery.toLowerCase()) // Filter by item name
   )
 
   return (
     <div className={`grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 items-center justify-center w-full mt-12 ${!itemData && 'h-full mt-56 grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1'} mb-16 md:mb-0`}>
-      {filteredItems.length > 0 && filteredItems.map((item, index) => {
+      {itemData ? filteredItems.length > 0 && filteredItems.map((item, index) => {
         return(
           <EachItem key={index} data={item} />
         )
-      })}
-      {!itemData && 
-      <div className="w-full flex flex-col items-center justify-center">
-        <h1 className="font-black text-5xl">MOHON MAAF</h1>
+      }) :
+      <div className="col-span-5 w-full flex flex-col items-center justify-center">
+        <h1 className="font-black text-3xl md:text-5xl">MOHON MAAF</h1>
         <h2 className="font-bold text-darkaqua">BELUM ADA IKAN YANG DIJUAL!</h2>
       </div>}
     </div>
