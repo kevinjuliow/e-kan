68af5bac642c993d9b697b3983b6acaf0c3c0235
stevanryan <stevanryan85@gmail.com>
2024-12-01 17:28:20 +0700

    Change profile link href in navbar

--- frontend/src/app/components/Navbar/Navbar.tsx
+++ frontend/src/app/components/Navbar/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
                 <div className="font-medium truncate">{session?.user?.email}</div>
               </div>
               <div className="py-1 cursor-pointer">
-                <Link href="/applicants/profile" className="block px-4 py-2 text-sm text-black hover:bg-gray-100 cursor-pointer">
+                <Link href="/profile" className="block px-4 py-2 text-sm text-black hover:bg-gray-100 cursor-pointer">
                   Profile
                 </Link>
               </div>
